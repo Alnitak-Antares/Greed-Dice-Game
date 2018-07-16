00b9227afbd60e88d3eec2609219f37069888bc9
zzzp <31700222+Alnitak-Antares@users.noreply.github.com>
2018-07-17 01:02:54 +0800

    梭哈先锋上线

--- AI.java
+++ AI.java
@@ -1,2 +1,13 @@
-public class AI {
+public class AI extends Player {
+    AI(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public boolean makeChoice(Dices dices)
+    {
+        dices.roll();
+        return true;
+    }
 }
