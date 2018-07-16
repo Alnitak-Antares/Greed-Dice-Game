bcac315699a83afa5aee5a99901b1e40682809c6
zzzp <31700222+Alnitak-Antares@users.noreply.github.com>
2018-07-17 01:01:39 +0800

    human's done
    human's done

--- Human.java
+++ Human.java
@@ -6,7 +6,7 @@ public class Human extends Player {
 
     @Override
     public boolean makeChoice(Dices dices) {
-        boolean choice = PlayView.choiceGet();
+        boolean choice = PlayView.getChoice();
         if(choice == true)
         {
             dices.roll();
