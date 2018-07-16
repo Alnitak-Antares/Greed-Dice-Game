bcb7c0f629aa7260a33f4fdc3f3cebb94a8d2a38
Mark <justfreefood@163.com>
2018-07-17 01:53:42 +0800

    hyc
    hyc

--- GameManager.java
+++ GameManager.java
@@ -4,29 +4,43 @@ public class GameManager {
         configureGame();
         startGame();
     }
-    private static player [] players;
-    private static int playerNum=0;
+    private static Player [] players = new Player[10];
+    private static int playerNum = 0;
 
     public static void configureGame()
     {
-        ConfigureView configureview = new ConfigureView();
-        boolean isConfigureEnd = false;
-        String playerName = new String();
+        boolean isConfigureEnd;
+        String playerName;
         int playerType;
         while(true) {
-            isConfigureEnd = configureview.getIfEnd();
+
+            isConfigureEnd = ConfigureView.getIfEnd();
             if(isConfigureEnd)
-                break;
-            playerType = configureview.getPlayerType();
-            playerName = configureview.getPlayerName();
-            players[playerNum++] = Player(playerType,playerName);
+            {
+                if(playerNum<2)
+                    System.out.println("请至少有两名玩家参加游戏");
+                else
+                    break;
+            }
+            if(playerNum == 10)
+            {
+                System.out.println("玩家最多为十人");
+                continue;
+            }
+            playerType = ConfigureView.getPlayerType();
+            playerName = ConfigureView.getPlayerName();
+            if(playerType == 0)
+                players[playerNum++] = new Human(playerName);
+            else
+                players[playerNum++] = new AI(playerName);
+
         }
     }
     public static void startGame()
     {
         Dices dices = new Dices();
         PlayerManager playermanager = new PlayerManager();
-        playermanager.start(players,dices);
+        playermanager.start(players,playerNum,dices);
     }
 
 }
