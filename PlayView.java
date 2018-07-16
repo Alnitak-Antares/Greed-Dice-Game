cd477ff1f71dc5b4e04bca5a0e453f6fec9f3dad
Mark <justfreefood@163.com>
2018-07-17 00:04:36 +0800

    00：01
    00:01

--- PlayView.java
+++ PlayView.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class PlayView {
-    public void diceScoreOut()
+    //private Scanner kbin = new Scanner(System.in);
+    public void isNotEnrolledOut()
     {
-
+        System.out.println("未入局");
     }
-    public void operation()
+    public void isEnrolledOut()
     {
-
+        System.out.println("入局");
+    }
+    public void playerTurnEndOut(int turnScore)
+    {
+        System.out.println("本局得分为"+turnScore+",玩家回合结束");
+    }
+    public void scoreUntilNowOut(String name,int turnScore)
+    {
+        System.out.println("玩家当前总得分为"+turnScore);
+    }
+    public void noDicesLeftOut()
+    {
+        System.out.println("没有剩余骰子,玩家回合结束");
+    }
+    public void playerTotalOut(String name,int turnScore)
+    {
+        System.out.println(name+"玩家当前总得分为"+turnScore);
+    }
+    public void playerGiveupOut(String name)
+    {
+        System.out.println(name+"玩家放弃投掷，玩家回合结束");
+    }
+    public void rollScoreOut(int rollScore)
+    {
+        System.out.println("本把得分为"+rollScore);
+    }
+    public void rollPointOut(String name,int[] point)
+    {
+        System.out.print(name+"玩家投掷出的骰子点数为");
+        for(int i=0;i<6;i++)
+        {
+            System.out.print(point+" ");
+        }
+        System.out.println(" ");
+    }
+    public void shuffledPlayerOut(String[] name)
+    {
+        for(int i=0;i<6;i++)
+        {
+            System.out.print(name[i]+" ");
+        }
+        System.out.println(" ");
     }
 }
--- PlayerManager.java
+++ PlayerManager.java
@@ -1,15 +1,21 @@
+import java.util.Random;
+
+
 public class PlayerManager {
     public void start(Player[] players,int playerNum,Dices dices,PlayView playview) {
-        sortPlayers(players);
+        sortPlayers(players,playerNum);
+        
         boolean gameOver = false;
         int playerIdx=-1;
+        /*全部人还没入局*/
         do {
             playerIdx = (playerIdx+1)%playerNum;
             setDices(dices);
+            Player curPlayer;
             while(true)
             {
                 boolean choice = players[playerIdx].makeChoice(dices);
-                Player curPlayer = players[playerIdx];
+                curPlayer = players[playerIdx];
                 if(choice)
                 {
                     playview.rollPointOut(curPlayer.getName(),dices.getPoint());
@@ -19,25 +25,31 @@ public class PlayerManager {
                     {
                         if(rollScore < 300) {
                             //”未入局“
+                        	playview.isNotEnrolledOut();
                             break;
                         }
-                        else
+                        else {
                             //”入局“
+                        	playview.isEnrolledOut();
                             curPlayer.setEnrolled();
+                        }
                     }
                     if(rollScore == 0)
                     {
                         //“本回合得分为零，玩家回合结束”
                         curPlayer.clearTurnScore();
+                        playview.playerTrunEndOut();
                         break;
                     }
                     curPlayer.addTurnScore(rollScore);
                     //“玩家本回合得分为turnScore”
+                    playview.scoreUntilNowOut(curPlayer.getTurnScore());
                     if(dices.getNumber() == 0)
                     {
                         //“没有剩余骰子，玩家回合结束”
-                        curPlayer.addTotalScore();
-                        curPlayer.clearTurnScore();
+                    	playview.noDicesLeftOut();
+                        //curPlayer.addTotalScore();
+                        //curPlayer.clearTurnScore();
                         //"xxw玩家当前总得分为："
                         break;
                     }
@@ -45,16 +57,24 @@ public class PlayerManager {
                 else
                 {
                     //"xx玩家放弃投掷，玩家回合结束"
-                    curPlayer.addTotalScore();
-                    curPlayer.clearTurnScore();
+                	playview.playerGiveupOut();
+                    //curPlayer.addTotalScore();
+                    //curPlayer.clearTurnScore();
                     //"xxw玩家当前总得分为："
                     break;
                 }
             }
+            //"玩家回合结束"
+            curPlayer.addTotalScore();
+            curPlayer.clearTurnScore();
+            //打印玩家信息
+
+            playview.playerTotalOut(curPlayer.getTotalScore());
             if(curPlayer.getTotalScore() >= 3000)
             {
                 //“宣布胜者，本局游戏结束”
                 gameOver = true;
+                playview.gameEndOut(curPlayer);
                 break;
             }
         }while(!gameOver);
@@ -63,7 +83,7 @@ public class PlayerManager {
         int now_roll_number=dices.getNumber();
         int[] rollscore=new int[now_roll_number];
         rollscore=dices.getPoint();
-        int[] roll=new roll [7];
+        int[] roll=new int[7];
         for(int score=1;score<=6;score++) roll[score]=0;
         int nowscore=0,decroll=0;
         if (roll[1]==6) {
@@ -103,11 +123,17 @@ public class PlayerManager {
 
 
     public void setDices(Dices dices) {
-
-
+    	dices.reset();
     }
-    public void sortPlayers(Player[] players)
+    public void sortPlayers(Player[] players,int playerNum)
     {
-
+    	Random rand = new Random();
+    	Player tmpPlayer=new Player();
+    	for(int i=0;i<playerNum;i++) {
+    		Player swapPlayer=Player[rand.nextInt(i+1)];
+    		tmpPlayer=swapPlayer;
+    		swapPlayer=Player[i];
+    		Player[i]=tmpPlayer;
+    	}
     }
 }
