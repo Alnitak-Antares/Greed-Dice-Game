28f36da3aa5736f3778c3aa30e1029ae03846b06
Mark <justfreefood@163.com>
2018-07-17 01:32:44 +0800

    hyc
    hyc

--- ConfigureView.java
+++ ConfigureView.java
@@ -4,16 +4,23 @@ public class ConfigureView {
 
     static private Scanner kbin = new Scanner(System.in);
 
-    static public int getIfEnd()
+    static public boolean getIfEnd()
     {
-        return kbin.nextInt();
+        System.out.println("是否停止设置并开始游戏？y/n");
+        boolean stop = (char)(kbin.nextByte()) == 'y';
+        if(stop == true)
+        System.out.println("——游戏开始——");
+        return stop;
+
     }
     static public int getPlayerType()
     {
+        System.out.println("请输入玩家类型：");
         return kbin.nextInt();
     }
     static public String getPlayerName()
     {
+        System.out.println("请输入玩家名字：");
         return kbin.nextLine();
     }
 }
