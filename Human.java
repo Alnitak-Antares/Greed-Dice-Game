47bdf6497d627fdcad7cd7407fb8e8a776c67d1f
zzzp <31700222+Alnitak-Antares@users.noreply.github.com>
2018-07-17 00:52:27 +0800

    human fall flat

--- Human.java
+++ Human.java
@@ -1,3 +1,18 @@
-public class Human {
+public class Human extends Player {
+    Human(String name)
+    {
+        super(name);
+    }
 
+    @Override
+    public boolean makeChoice(Dices dices) {
+        boolean choice = PlayView.choiceGet();
+        if(choice == true)
+        {
+            dices.roll();
+            return true;
+        }
+        else
+            return false;
+    }
 }
