48e16c1678ab6f633d26a0dc8470a3527cbc490e
zzzp <31700222+Alnitak-Antares@users.noreply.github.com>
2018-07-18 03:15:08 +0800

    Add files via upload

---
+++ Dices.java
@@ -0,0 +1,32 @@
+package greed_game;
+import java.util.Random;
+
+public class Dices {
+    private int diceNumber = 6;
+    private int dicePoint[] = new int[6];
+
+    public void roll()
+    {
+        Random rand = new Random();
+        for(int i = 0;i < diceNumber;i++)
+        {
+            dicePoint[i] = rand.nextInt(6)+1;
+        }
+    }
+    public int getNumber()
+    {
+        return diceNumber;
+    }
+    public void reset()
+    {
+        diceNumber=6;
+    }
+    public int[] getPoint()
+    {
+        return dicePoint;
+    }
+    public void removeDices(int removenumber)
+    {
+        diceNumber -= removenumber;
+    }
+}
